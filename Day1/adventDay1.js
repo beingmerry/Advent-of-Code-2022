cf3eafb884dcb310d3380d2901069169523f66e4
Ben <ben.merryman@gmail.com>
2022-12-01 19:03:36 -0500

    data coming in, mappedData at console

--- Day1/adventDay1.js
+++ Day1/adventDay1.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+const reader = new FileReader()
+const fileElement = document.getElementById('fileInput')
+const file = fileElement.files[0]
+reader.readAsText(file)
+let data = []
+reader.addEventListener('load', () => {
+    data = reader.result
+    mappedData = data.split("\n")
+    console.log(mappedData)
+    
+})
