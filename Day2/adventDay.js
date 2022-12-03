e1d1224edf27f3a1abf06b7c87b47794e7181911
Ben <ben.merryman@gmail.com>
2022-12-03 01:57:23 -0500

    day 2 both stars done

--- Day2/adventDay.js
+++ Day2/adventDay.js
@@ -12,100 +12,124 @@ const processing = document.getElementById('processing')
 const coll = document.getElementsByClassName('collapsible')
 
 const instructions = document.getElementsByClassName('instructions')
-
-let file = 'Select a file with the HTML element!'
-
-const scoreRockPaperScissors = (firstLetter, secondLetter) => {
-  debugger
+// <-------------------------------------------------------------------------------->
+// ⭐ Star 1
+const gameLoop = (firstLetter, secondLetter) => {
   const letterToAction = {
-    'A': 'rock',
-    'B': 'paper',
-    'C': 'scissors',
-    'X': 'rock',
-    'Y': 'paper',
-    'Z': 'scissors'
+    A: 'rock',
+    B: 'paper',
+    C: 'scissors',
+    X: 'rock',
+    Y: 'paper',
+    Z: 'scissors'
   }
-  let score = 0;
-  // ❌🏗️🔮🤣 Add scored elements here, just write to the elements in this function!
+  // X means need to lose
+  // Y means need to draw
+  // Z means need to win
+  const opponentAction = letterToAction[firstLetter]
+  const playerAction = letterToAction[secondLetter]
   // 🎯still want to pass back the values and the names for showcasing the data
   const playerStateTable = {
-    'rock': 1, // Rock 1
-    'paper': 2, // Paper 2
-    'scissors': 3  // Scissors 3
+    rock: 1, // Rock 1
+    paper: 2, // Paper 2
+    scissors: 3 // Scissors 3
   }
-  const opponentStateTable = {
-    'A': {
-      'X': 1,
-      'Y': 2,
-      'Z': 3
+
+  // <-------------------------------------------------------------------------------->
+  //Track wins vs ties vs losses, set score, AND set textContent for GroupedList
+  const gameStateTable = {
+    rock: {
+      rock: 3,
+      paper: 0,
+      scissors: 6
     },
-    'B': {
-      'X': 1,
-      'Y': 2,
-      'Z': 3
+    paper: {
+      rock: 6,
+      paper: 3,
+      scissors: 0
     },
-    'C': {
-      'X': 1,
-      'Y': 2,
-      'Z': 3
+    scissors: {
+      rock: 0,
+      paper: 6,
+      scissors: 3
     }
   }
-    const resultsObj = {
-      score: score, 
-      gameStateOpponent: gameStateOpponent, 
-      gameStateYou: gameStateYou
-    }
-    return resultsObj
+  // <-------------------------------------------------------------------------------->
+  // ✨ Star 2  // Reversed points from desired to find LOSING states for player
+  const letterToPointsReverse = {
+    X: 6,
+    Y: 3,
+    Z: 0
+  }
+  const letterToPointsOriginal = {
+    X: 0,
+    Y: 3,
+    Z: 6
+  }
+  const playerScoreNeeded = letterToPointsReverse[secondLetter]
+  const opponentReversedTable = gameStateTable[opponentAction]
+  const starTwoPlayerAction = Object.keys(opponentReversedTable).find(
+    key => opponentReversedTable[key] === playerScoreNeeded
+  )
+  // ✨ Star Two Calcs
+  const starTwoActionScore = playerStateTable[starTwoPlayerAction]
+  const gameStarTwo = starTwoActionScore + letterToPointsOriginal[secondLetter]
+
+  // ⭐ Star One Calcs
+  const starOneActionScore = playerStateTable[playerAction]
+  const starOneGameScore = gameStateTable[playerAction][opponentAction]
+  const gameStarOne = starOneActionScore + starOneGameScore
+
+  const opponentVerb =
+    starOneGameScore === 0
+      ? 'beats'
+      : starOneGameScore === 3
+      ? 'tied'
+      : 'loses to'
+  const groupedItem = document.createElement('li')
+  groupedItem.textContent = `opponent ${opponentAction} ${opponentVerb} ${playerAction} | 🎅 ${starOneGameScore} game points and ${starOneActionScore} action points for you!`
+  groupedList.appendChild(groupedItem)
+  const sortedItem = document.createElement('li')
+  sortedItem.textContent = `goal is ${letterToPointsOriginal[secondLetter]} points target, you play ${starTwoPlayerAction} and they play ${opponentAction}`
+  sortedList.appendChild(sortedItem)
+  const results = {
+    gameStarOne: gameStarOne,
+    gameStarTwo: gameStarTwo
+  }
+  return results
 }
 
+// <-------------------------------------------------------------------------------->
+// 📂 Get the file!
+let file = 'Select a file with the HTML element!'
 startButton.addEventListener('click', () => {
   processing.textContent = '⌛ Processing...'
   file = fileInput.files[0]
   reader.readAsText(file)
 })
-
+// <-------------------------------------------------------------------------------->
 reader.addEventListener('load', () => {
   data = reader.result
   mappedData = data.split('\n')
-  // let elfCals = []
-  // let elfCalsIndex = 0 let count
-  let count = 0
-  for (let i = 0; i < mappedData.length; i++) {
+  let totalFirstStarScore = 0
+  let totalSecondStarScore = 0
+  for (let i = 0; i < mappedData.length - 1; i++) {
     const currentLine = mappedData[i]
     const inputListItem = document.createElement('li')
     const [firstLetter, secondLetter] = currentLine.split(' ')
-    
     inputListItem.textContent = `${firstLetter} ${secondLetter}`
     inputList.appendChild(inputListItem)
-    // 🎯 Need to re write this loop to sort # of A, B, C... and X, Y, Z
-    const resultsObj = scoreRockPaperScissors(firstLetter, secondLetter)
-
-    
-    // if (isNaN(cals)) {
-    //   const groupedItem = document.createElement('li')
-    //   groupedItem.textContent = elfCals[elfCalsIndex]
-    //   groupedList.appendChild(groupedItem)
-    // } else {
-    //   elfCals[elfCalsIndex] === undefined
-    //     ? elfCals.push(cals)
-    //     : (elfCals[elfCalsIndex] = elfCals[elfCalsIndex] + cals)
-    // }
-  }
-  // 🎯 Need to re write for Rock Paper Scissors solution 1
-  // const sortedElfCals = elfCals.sort((a, b) => b - a)
-
-  // sortedElfCals.forEach(listItem => {
-  //   const sortedItem = document.createElement('li')
-  //   sortedItem.textContent = listItem
-  //   sortedList.appendChild(sortedItem)
-  // })
-  // const solutionSecondStar =
-  //   sortedElfCals[0] + sortedElfCals[1] + sortedElfCals[2]
-  // solution.textContent = `⭐ Answer 1 = ${sortedElfCals[0]} | ✨ Answer 2 = ${solutionSecondStar}`
-  // processing.textContent = '✅ Done! (does not guarantee a correct solution)'
+    const results = gameLoop(firstLetter, secondLetter)
+    totalFirstStarScore += results.gameStarOne
+    totalSecondStarScore += results.gameStarTwo
+  } 
+  const solutionFirstStar = totalFirstStarScore
+  const solutionSecondStar = totalSecondStarScore
+  solution.textContent = `⭐ Answer 1 = ${solutionFirstStar} | ✨ Answer 2 = ${solutionSecondStar}`
+  processing.textContent = '✅ Done! (does not guarantee a correct solution)'
 })
-
-// 🏗️ Visual only functions below
+// <-------------------------------------------------------------------------------->
+// 💄 Visual functions
 // 1️⃣ Adds toggle to the results data for expanding and collapsing
 for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener('click', () => {
@@ -118,3 +142,4 @@ for (let i = 0; i < coll.length; i++) {
     }
   })
 }
+// <-------------------------------------------------------------------------------->
